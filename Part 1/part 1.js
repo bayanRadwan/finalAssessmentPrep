a1e5e62ca0f9a2a15a3f93d0fb0f790a3ad7bbc3
bayanRadwan <bayanwaleedoo@gmail.com>
2016-12-21 13:00:13 +0200

    answers

--- Part 1/part 1.js
+++ Part 1/part 1.js
@@ -13,20 +13,54 @@
 	c- create a function called avergeStudents() to calculate the average age of your class students, is it possible to pass all your class students one by one ? think of a way to pass them in your function
 */
 // write your code here ...
+function students (name,age,education,nationality){
+	return {
+		name:name,
+		age:age,
+		education:education,
+		nationality:nationality
+	}
+}
+var fr1=students("aya",21,"computer","iraqian");
+var fr2=students("duaa",23,"software engineering","jordanian")
 
-
-
-
+function showFriend(frinedData){
+	var x=""
+	for (var key in frinedData){
+		x=frinedData.name+" "+"with the age of"+" "+frinedData.age+" "+ "and with"+" "+frinedData.education+" "+"education"
+	}
+	return x
+}
+var x=[fr1,fr2]
+function avergeStudents (x){
+	var sum =0
+	var avg=0
+for (var i =0 ;i<x.length;i++){
+	sum+=x[i][age]
+	avg=sum/x.length
+}
+return avg
+}
 /*
  b-create a function called rangeSquared in which it will accept two parameters, and will output the squared numbers between these two parameter if the number is even 
 	in order to square the numbers create a function called square and call it inside rangeSquared function
 	rangeSquared(2)// [4];
 	rangeSquared(3) // null
-	rangeSquared(2,10)// [4,16,12,16,100];
+	rangeSquared(2,10)// [4,16,36,64,100];
 */
 // write your code here ...
 
-
+function rangeSquared (n1,n2){
+	var arr=[]
+	if (n2===undefined || n1 %2!==0){
+		return n1
+	}
+	for ( var i =n1;i<=n2;i+2){
+		arr.push(i*i)
+		i+=2
+	}
+	return arr
+}
 
 
 /* c- Find all leaders in an array. A leader is an element larger than all elements to the right of it.
@@ -35,4 +69,20 @@
 */
 
 // write your code here ....
+function leader (array){
+	var max = array[0];
+	var newarr=[];
+	for ( var i =0 ; i<array.length;i++){
+		if (max>array[i+1]){
+			newarr.push(array[i])
+		}
+		i++
+		if(array[i]>array[i+1]){
+			i++
+		newarr.push(array[i+1])
+	}
+	}
+		return newarr
+}
+
 
--- Part 2/Part 2.js
+++ Part 2/Part 2.js
@@ -13,6 +13,7 @@
  	a - we need to create the players using OOP the player declaration
     should take the name of the player  	==> var player1 = Player("jony")
 
+
   b - after that we need function to add the other info for the player with addInfo function
 		==> player1.addInfo(age, position, level, availability );
 
@@ -31,4 +32,121 @@
 		sortPalyerBy(arrayOfPlayers, "name");
  */
 
-//writ your code here .....
+//writ your code here .....function each(coll, func) {
+	function each(coll, func) {
+     if (Array.isArray(coll)) {
+         for (var i = 0; i < coll.length; i++) {
+                func(coll[i], i);
+         }
+     } else {
+         for (var key in coll) {
+                func(coll[key], key);
+         }
+     }
+}
+              function reduce(array, f, acc) {
+            if (acc === undefined) {
+              acc = array[0];
+              array = array.slice(1);
+            }
+            each(array, function(element, i) {
+              acc = f(acc, element, i);
+            });
+            return acc;
+          }
+          function filter(array, predicate) {
+     var acc = [];
+     each(array, function(element, i) {
+          if (predicate(element, i)) {
+                acc.push(element);
+          }
+     });
+     return acc;
+}
+function map(coll, f) {
+    var acc = [];
+    if (!Array.isArray(coll)) {
+        acc = {};
+    }
+    each(coll, function(element, key) {
+    acc[key] = f(element, key);
+    });
+    return acc;
+}
+
+ 	// a - we need to create the players using OOP the player declaration
+  //   should take the name of the player  	==> var player1 = Player("jony")
+
+function players (initial){
+	var counter {};
+	counter.player=player;
+	counter.addInfo=addInfo;
+	counter.increaseLeve=increaseLeve;
+	counter.isAvailable=isAvailable;
+	counter.decrease=decrease;
+	counter.sortPalyersAge=sortPalyersAge;
+	counter.sortPalyersName=sortPalyersName;
+	return counter;
+}
+var player =function (name){
+return name
+} 
+
+  // b - after that we need function to add the other info for the player with addInfo function
+		// ==> player1.addInfo(age, position, level, availability );
+var addInfo =function (age,position,level,availability){
+	return this.player{
+		age:age,
+		position:position,
+		level:level,
+		availability:availability
+		 };
+}
+// c- and create another function to increase the the level for the player by n value 
+// 		==> player1.increaseLevel(4);
+ var increaseLevel =function(n){
+ 	var x=this.player[level];
+ 	return x+=n
+ }
+ // d - we need another function to check if the player is available or not it should return true or false
+	// 	==> player1.isAvailable();  true / false
+	var isAvailable = function(){
+		var x=this.player(name)
+		if(name===undefined){
+			return false;
+		}
+		return true;
+	}
+
+// e - then we need to create 4 players and add the info for them and put them inside the arrayOfPlayers
+var player1=this.player("bayan");
+var player2=this.player("aya");
+var player3=this.player("mohammad");
+var player4=this.player("ahmad");
+
+
+// f - write function to iterate over the all players to decrease the level for the player if age larger the 30
+
+var decrease=function(){
+var x=this.player
+each(X,function(elem,key){
+	if (elem[age]>30){
+		elem[level]-1
+	}
+})
+}
+
+
+  // g - sort the players in the arrayOfPlayers by the key
+		// sortPalyerBy(arrayOfPlayers, "age");
+		// sortPalyerBy(arrayOfPlayers, "name");
+		var sortPalyersAge=function(){
+			var x=this.player[age]
+			x.sort(function(a,b){
+				return list[a]-list[b]})
+		}
+		var sortPalyersName=function(){
+			var x=this.player[name]
+			x.sort(function(a,b){
+				return list[a]-list[b]})
+		}
